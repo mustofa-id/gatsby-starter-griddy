d5590cfb41688f8722afa1a6a7a4c374b45a713a
hm <hm.dev7@gmail.com>
2019-07-14 10:44:31 +0700

    Extract strokeWidth props

--- src/components/widget/icon.js
+++ src/components/widget/icon.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import feather from '../../assets/icons/feather.svg'
 
-const Icon = ({ name, size, width, height, color, style }) => (
+const Icon = ({ name, size, width, height, color, style, strokeWidth }) => (
   <svg style={style}
     width={size || width}
     height={size || height}
     fill='none'
     stroke={color || 'currentColor'}
-    strokeWidth='2'
+    strokeWidth={strokeWidth || '2'}
     strokeLinecap='round'
     strokeLinejoin='round'>
     <use xlinkHref={`${feather}#${name}`} />
